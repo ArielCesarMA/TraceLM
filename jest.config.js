1e426b50cb29c3068ef6068b81e0641ed4cf819b
Ariel Cesar Abaoag <abaoagarielcesar@gmail.com>
2026-06-16 14:05:10 +0800

    chore: implement phase 2 automation pipeline

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,20 @@ module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
     '!src/extension.ts'
-  ]
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 5,
+      functions: 15,
+      lines: 12,
+      statements: 13
+    }
+  }
 };
